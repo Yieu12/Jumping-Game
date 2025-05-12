785aaef09c99725087ca03915d93701476c384fe
Yieu12 <57107178+Yieu12@users.noreply.github.com>
2025-05-12 02:11:21 -0700

    Update player.gd

--- Chars/Scripts/player.gd
+++ Chars/Scripts/player.gd
@@ -15,22 +15,13 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Handles jump if buttons are pressed.
 	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.animation = "jump"
 	elif velocity.y == 0:
 		$AnimatedSprite2D.animation = "stand"
 		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
 func _on_area_2d_body_entered(body):
